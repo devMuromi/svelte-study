51d34fa8438b49c957f2bd1b28ee01012441a8a4
devMuromi <devmuromi@gmail.com>
2022-12-19 16:20:27 +0900

    INIT, ~lecture 12

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+  let name = "muromi";
+  let age = 21;
+
+  $: uppercaseName = name.toUpperCase();
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = "rika";
+  }
+</script>
+
+<main>
+  <h1>Hello {uppercaseName}!, my age is {age}</h1>
+  <button on:click={incrementAge}>Change Age</button>
+  <button on:click={changeName}>Change Name</button>
+
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
