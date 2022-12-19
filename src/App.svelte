2ff6ae2075eafc7529199eaf37328d79d3ab83ef
devMuromi <devmuromi@gmail.com>
2022-12-19 17:28:57 +0900

    ADD lecture 16

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,54 @@
 <script>
-  let name = "muromi";
-  let age = 21;
+	import ContactCard from './ContactCard.svelte';
 
-  $: uppercaseName = name.toUpperCase();
+	let name = 'muromi';
+	let age = 21;
 
-  $: if (name == "rika") {
-    age = 1;
-  }
+	$: uppercaseName = name.toUpperCase();
 
-  function incrementAge() {
-    age += 1;
-  }
+	$: if (name == 'rika') {
+		age = 1;
+	}
 
-  //   function changeName() {
-  //     name = "rika";
-  //   }
+	function incrementAge() {
+		age += 1;
+	}
 
-  //   function nameInput(e) {
-  //     name = e.target.value;
-  //   }
+	//   function changeName() {
+	//     name = "rika";
+	//   }
+
+	//   function nameInput(e) {
+	//     name = e.target.value;
+	//   }
 </script>
 
 <main>
-  <h1>Hello {uppercaseName}!, my age is {age}</h1>
-  <button on:click={incrementAge}>Change Age</button>
-  <!-- <button on:click={changeName}>Change Name</button> -->
-  <input type="text" bind:value={name} />
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+	<h1>Hello {uppercaseName}!, my age is {age}</h1>
+	<button on:click={incrementAge}>Change Age</button>
+	<!-- <button on:click={changeName}>Change Name</button> -->
+	<input type="text" bind:value={name} />
+	<ContactCard />
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
