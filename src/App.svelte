9a859f52ce1ecf10bbe2f0e470c4ddf291791181
devMuromi <devmuromi@gmail.com>
2022-12-21 15:35:57 +0900

    ADD lecture 49

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,16 @@
 <script>
 	import Header from './UI/Header.svelte';
-	import MeetupItem from './Meetups/MeetupItem.svelte';
 
-	const meetups = [
+	import MeetupGrid from './Meetups/MeetupGrid.svelte';
+
+	let title = '';
+	let subtitle = '';
+	let address = '';
+	let email = '';
+	let description = '';
+	let imageUrl = '';
+
+	let meetups = [
 		{
 			id: 'm1',
 			title: 'Coding Bootcamp',
@@ -24,18 +32,55 @@
 			email: 'aaa@example.com'
 		}
 	];
+
+	function addMeetup() {
+		const newMeetup = {
+			id: Math.random().toString(),
+			title: title,
+			subtitle: subtitle,
+			address: address,
+			email: email,
+			description: description,
+			imageUrl: imageUrl
+		};
+		meetups = [newMeetup, ...meetups];
+	}
 </script>
 
 <Header />
-
-<section id="meetups">
-	{#each meetups as meetup}
-		<MeetupItem {meetup} />
-	{/each}
-</section>
+<main>
+	<form on:submit|preventDefault={addMeetup}>
+		<div class="form-control">
+			<label for="title">Title</label>
+			<input type="text" id="title" bind:value={title} />
+		</div>
+		<div class="form-control">
+			<label for="subtitle">Subtitle</label>
+			<input type="text" id="subtitle" bind:value={subtitle} />
+		</div>
+		<div class="form-control">
+			<label for="address">Address</label>
+			<input type="text" id="address" bind:value={address} />
+		</div>
+		<div class="form-control">
+			<label for="imageUrl">Image URL</label>
+			<input type="text" id="imageUrl" bind:value={imageUrl} />
+		</div>
+		<div class="form-control">
+			<label for="email">E-Mail</label>
+			<input type="email" id="email" bind:value={email} />
+		</div>
+		<div class="form-control">
+			<label for="description">Description</label>
+			<textarea rows="3" id="description" bind:value={description} />
+		</div>
+		<button type="submit">Save</button>
+	</form>
+	<MeetupGrid {meetups} />
+</main>
 
 <style>
-	#meetups {
+	main {
 		margin-top: 5rem;
 	}
 </style>
--- src/Meetups/MeetupItem.svelte
+++ src/Meetups/MeetupItem.svelte
@@ -1,21 +1,49 @@
 <script>
+	export let meetup;
 </script>
 
 <article>
 	<header>
-		<h1>TITLE</h1>
-		<h2>SUBTITLE</h2>
+		<h1>{meetup.title}</h1>
+		<h3>{meetup.subtitle}</h3>
+		<h2>{meetup.address}</h2>
 	</header>
 	<div class="image">
-		<img src="" alt="" />
+		<img src={meetup.imageUrl} alt="" />
 	</div>
 	<div>
 		<div class="content">
-			<p />
+			<p>{meetup.description}</p>
 		</div>
 		<footer>
+			<a href="mailto:{meetup.email}">Contact</a>
 			<button>Show Details</button>
 			<button>Favorite</button>
 		</footer>
 	</div>
 </article>
+
+<style>
+	article {
+		box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+		border-radius: 5px;
+		background: white;
+		margin: 1rem;
+	}
+	header,
+	.content,
+	.footer {
+		padding: 1rem;
+	}
+	.image {
+		width: 100%;
+		height: 14rem;
+	}
+	p {
+		font-size: 1.25rem;
+		margin: 0;
+	}
+	div {
+		text-align: right;
+	}
+</style>
