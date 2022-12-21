a43fe3ba0be46d560a420cfc770359043030fc9d
devMuromi <devmuromi@gmail.com>
2022-12-21 11:29:20 +0900

    ADD lecture 44

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,28 @@
 <script>
 	import Header from './UI/Header.svelte';
+
+	const meetups = [
+		{
+			id: 'm1',
+			title: 'Coding Bootcamp',
+			subtitle: 'Learn to code in 2 hours',
+			description: 'In this meetup, we will have experts that teach you how to code!',
+			imageUrl:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Codecademy_logo.svg/1200px-Codecademy_logo.svg.png',
+			address: '27th Nerd Road, 32523 New York',
+			email: 'blahblah@example.com'
+		},
+		{
+			id: 'm2',
+			title: 'Swimming',
+			subtitle: 'Learn to swim in 2 hours',
+			description: 'In this meetup, we will have experts that teach you how to swim!',
+			imageUrl:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Codecademy_logo.svg/1200px-Codecademy_logo.svg.png',
+			address: '27th Nerd Road, 32523 New York',
+			email: 'aaa@example.com'
+		}
+	];
 </script>
 
 <Header />
